2b09dd1cbefbbb5696d97269d14f28a5e99f0be8
BigFat0 <76102452+BigFat-0@users.noreply.github.com>
2025-04-05 20:11:39 +0100

    Update urls.py
    urls changed home to login

--- webapps2025/urls.py
+++ webapps2025/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from payapp.views import CurrencyConversionView  # Import the view
+from payapp.views import CurrencyConversionView
+from register.views import user_login
 
 urlpatterns = [
+    path('', user_login, name='home'),
     path('webapps2025/admin/', admin.site.urls),  # Admin path
     path('webapps2025/', include('register.urls')),  # Register app URLs
     path('webapps2025/', include('payapp.urls')),    # Payapp URLs
